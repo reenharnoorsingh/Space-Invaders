ac509de52f133e812f62b7dbf917ff66187197a4
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-12-25 19:40:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,16 +5,12 @@ import time
 import random
 
 # loading images
-RED_SHIP = pygame.image.load(os.path.join(
-    "assets", "pixel_ship_red_small.png"))
-GREEN_SHIP = pygame.image.load(os.path.join(
-    "assets", "pixel_ship_green_small.png"))
-BLUE_SHIP = pygame.image.load(os.path.join(
-    "assets", "pixel_ship_blue_small.png"))
+RED_SHIP = pygame.image.load("assets\pixel_ship_red_small.png")
+GREEN_SHIP = pygame.image.load("assets\pixel_ship_green_small.png")
+BLUE_SHIP = pygame.image.load("assets\pixel_ship_blue_small.png")
 
 # player ship
-YELLOW_SHIP = pygame.image.load(
-    os.path.join("assets", "pixel_ship_yellow.png"))
+YELLOW_SHIP = pygame.image.load("assets\pixel_ship_yellow.png")
 
 # lasers
 RED_LASER = pygame.image.load("assets\pixel_laser_red.png")
