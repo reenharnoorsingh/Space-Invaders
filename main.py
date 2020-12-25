d59b6184b8fd6776d25a5091853e7ecb8f012665
Harnoor Singh Reen <54823204+reenharnoorsingh@users.noreply.github.com>
2020-12-25 19:37:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import time
 import random
 
 # loading images
-RED_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
-GREEN_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
-BLUE_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+RED_SHIP = pygame.image.load(os.path.join(
+    "assets", "pixel_ship_red_small.png"))
+GREEN_SHIP = pygame.image.load(os.path.join(
+    "assets", "pixel_ship_green_small.png"))
+BLUE_SHIP = pygame.image.load(os.path.join(
+    "assets", "pixel_ship_blue_small.png"))
 
-#player ship
-YELLOW_SHIP = pygame.image.load(os.path.join("assets","pixel_ship_yellow.png"))
+# player ship
+YELLOW_SHIP = pygame.image.load(
+    os.path.join("assets", "pixel_ship_yellow.png"))
 
-#lasers
+# lasers
 RED_LASER = pygame.image.load("assets\pixel_laser_red.png")
-
+GREEN_LASER = pygame.image.load("assets\pixel_laser_green.png")
+BLUE_LASER = pygame.image.load("assets\pixel_laser_blue.png")
+YELLOW_LASER = pygame.image.load("assets\pixel_laser_yellow.png")
